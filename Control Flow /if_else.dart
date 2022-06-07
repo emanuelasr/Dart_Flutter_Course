ff64a34f543735a7baa808e198946613d31350f2
EmanuelMac <emanuelasrm@hotmail.com>
2022-06-07 19:08:29 -0300

    Section 4 - Control Flow

---
+++ Control Flow /if_else.dart
@@ -0,0 +1,16 @@
+/// if else statements are very importan because they allow us to write programs that make decisions
+/// and run some code based on some conditions. so if a certain condition is true,
+/// we're going to do some things, otherwise we'll do some other things. heres an example
+void main() {
+  var age = 10; // defining a age.
+
+  if (age <= 10) {
+    // here it s the conditional flow if the condition inside the parenthesis, are true
+    // then the program will run 'kids'
+    print('kids');
+  } else {
+    // if the condition is false
+    // then the program will run, 'young'
+    print('young');
+  }
+}
